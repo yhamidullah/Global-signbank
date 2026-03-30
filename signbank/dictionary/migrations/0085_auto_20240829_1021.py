169e1309e861745e6bef3888c25ffde1c5da4f08
HAMIDULLAH Yasser <13780954+yhamidullah@users.noreply.github.com>
2026-03-30 19:25:47 +0200

    fix 0085 migration

--- signbank/dictionary/migrations/0085_auto_20240829_1021.py
+++ signbank/dictionary/migrations/0085_auto_20240829_1021.py
@@ -6,8 +6,12 @@ from guardian.models import UserObjectPermission, GroupObjectPermission
 
 
 def remove_can_view_dataset(apps, schema_editor):
-    view_dataset_perm = Permission.objects.get(codename='view_dataset')
-    can_view_dataset_perm = Permission.objects.get(codename='can_view_dataset')
+    # skip to fix it
+    try:
+        view_dataset_perm = Permission.objects.get(codename='view_dataset')
+        can_view_dataset_perm = Permission.objects.get(codename='can_view_dataset')
+    except Permission.DoesNotExist:
+        return
 
     # User - Permission
     for user in User.objects.filter(user_permissions=can_view_dataset_perm)\
